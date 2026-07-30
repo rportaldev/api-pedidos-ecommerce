862480a753c408fbef707819a9b88837bfa3c42b
Dev Zero <litkaponii@gmail.com>
2026-07-29 21:36:27 -0500

    feat: creación inicial del proyecto API Pedidos E-commerce

---
+++ src/main/java/com/rportaldev/apipedidosecommerce/ApiPedidosEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.rportaldev.apipedidosecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPedidosEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPedidosEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rportaldev/apipedidosecommerce/ApiPedidosEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rportaldev.apipedidosecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPedidosEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
